67f5e0f3c69a5826dcd9f81daae2a5e3d14210ab
Amit Chauhan <Amit Chauhan@TTNPL-5366>
2024-08-15 13:30:48 +0530

    episode-8

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,30 @@
-const About = () => {
-    return (
-          <div>
-            <h1>This is About us Page</h1>
-          </div>
-    );
+import Userclass from "./UserClass";
+import React from "react";
+
+class About extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+              <h1>This is About us Page</h1>
+              {<Userclass name="AMit user class" mobile = "881111111" />}
+            </div>
+      );
+    }
 }
+
+
+// const About = () => {
+//     return (
+//           <div>
+//             <h1>This is About us Page</h1>
+//             {<Userclass name="AMit user class" mobile = "881111111" />}
+//           </div>
+//     );
+// }
 
 export default About;
---
+++ src/components/UserClass.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+class Userclass extends React.Component {
+  constructor(props){
+     super(props);
+     this.state = {
+        userInfo:{
+            name: "",
+            login: ""
+        }
+     }
+    }
+
+    async componentDidMount(){
+      const data = await fetch("https://api.github.com/users/amit-chauhan29");
+      const jsonData = await data.json();
+      this.setState({
+        userInfo : jsonData
+      });
+    }
+
+    render(){
+        const {name, mobile} = this.props;
+        const {count, count2} = this.state;
+        return (
+            <div>
+                <h1>Amit Chauhan</h1>
+                <h1>{this.state.userInfo.name}</h1>
+                <h1>{this.state.userInfo.login}</h1>
+                <img src={this.state.userInfo.avatar_url}></img>
+                <h1>Count: {count}</h1>
+                <button onClick={() => {
+                    this.setState({
+                        count : this.state.count +1,
+                        count2 : count2 + 2
+                    });
+                }}>Increase Count</button>
+                <h1>Count2: {count2}</h1>
+            </div> 
+        )
+    }
+};
+
+export default Userclass;
